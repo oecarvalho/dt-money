66d63dce8a732794d5978a9fce80673ff6734f75
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-09-19 17:53:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
+
+function App() {
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+        <h1>Teste</h1>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
