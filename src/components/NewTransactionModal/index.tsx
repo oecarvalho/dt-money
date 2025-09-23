28dc85906ea6c0d580a641ec7e12b4c5e3f4d52b
Felipe <felipe.carvalho.dm@gmail.com>
2025-09-23 02:21:04 -0300

    finalizando estilização visual da página

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { Overlay, Content, CLoseButton } from './styles'
-import { X } from 'phosphor-react'
+import { Overlay, Content, CLoseButton, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 
 export function NewTransactionModal() {
@@ -17,6 +17,18 @@ export function NewTransactionModal() {
                     <input type="text" placeholder='Descrição' required/>
                     <input type="number" placeholder='Preço' required/>
                     <input type="text" placeholder='Categoria' required/>
+
+                    <TransactionType>
+                        <TransactionTypeButton variant='income' value='income'>
+                            <ArrowCircleUp size={24}/>
+                            Entrada
+                        </TransactionTypeButton>
+
+                        <TransactionTypeButton variant='outcome' value='outcome'>
+                            <ArrowCircleDown size={24}/>
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
 
                     <button type='submit'>Cadastrar</button>
                 </form>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Overlay = styled(Dialog.Overlay)`
     position: fixed;
@@ -66,4 +67,46 @@ export const CLoseButton = styled(Dialog.Close)`
     cursor: pointer;
     color: ${props => props.theme['gray-500']};
     line-height: 0;
+`
+
+export const TransactionType = styled(RadioGroup.Root)`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+    margin-top: 0.5rem;
+`
+
+interface TransactionTypeButtonProps {
+    variant: 'income'|'outcome'
+}
+
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
+    background: ${props => props.theme['gray-700']};
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    border-radius: 6px;
+    cursor: pointer;
+    border: 0;
+    color: ${props => props.theme['gray-300']};
+
+    svg{
+        color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+    }
+
+    &[data-state='checked']{
+        color: ${props => props.theme['white']};
+        background: ${props => props.variant === 'income' ? props.theme['green-500'] : props.theme['red-500']};
+        transition: .3s;
+
+        svg{
+            color: ${props => props.theme['white']};
+        }
+    }
+
+    &[data-state='unchecked']{
+        background: ${props => props.theme['gray-600']};
+    }
 `
